69be55895284a818df0094ab4da8735ee00bd385
nr072 <26773186+nr072@users.noreply.github.com>
2019-08-09 21:36:59 +0600

    shorten URL by changing right-click's target's href
    Detect right-click on HTML <a> tag, trim its URL down, and then write the 
    modified URL to the target's `href`. Now, simply clicking "Copy link location" 
    from contextmenu copies the trimmed down URL. 
     
    Need to change approach, since this CHANGES the content of a page (although 
    temporarily – everything resets on reload).

--- from-notifications.js
+++ from-notifications.js
@@ -1,56 +1,37 @@
-// copy-paste in browser terminal
+// copy-paste code in browser terminal
 //
 // right click on FB query-full long link to shorten to minimum
 // e.g., "fb.com/groups/myGroup/?multi_permalinks=xxxx&click_id=xxxx" to "fb.com/groups/myGroup/"
-// only right click on <a> tag for this to work
+// right-click target must be HTML <a> tag only, not children
 
 {
     document.onmousedown = function(e) {
         
-        // detect right-click
-        if (e.which==3) {
+        // detect right-click on HTML <a> tag that has a URL
+        if (e.which==3 && e.target.tagName=="A" && e.target.href) {
             
-            // only works when right-clicked on <a> tag, not on any child
-            if (e.target.tagName=="A") {
-                
-                let t = document.createElement("textarea");
-                
-                // either takes the URL or nothing is copied
-                let v = e.target.href || "";
-                
-                // manually entered delimiters
-                let args = [
-                    ["facebook.com/groups/", "/"],
-                    ["/posts/",              "?"],
-                ];
-                
-                // split URL using parts and then add again to make URL excluding excess
-                // if not of one type, then must be another; if none at all, nothing is copied
-                for (let c=0; c<args.length; ++c) {
-                    v = v.split(args[c][0]).length==2
-                        ? (v.split( args[c][0] )[0] + args[c][0] + v.split( args[c][0] )[1].split( args[c][1] )[0])
-                        : "";
-                    if (v) 
-                        break;
-                }
-                
-                t.value = v;
-                
-                // "display:none" seems to have some problem with "select()"
-                t.style.height = 0;
-                
-                document.body.appendChild(t);
-                
-                // copy modified link (currently the content of the invisible text-area)
-                t.select();
-                document.execCommand("copy");
-                
-                document.body.removeChild(t);
-                
-                // scroll up, since text-area is added at the end of body and view is scrolled down
-                window.scrollTo(0, 0);
-                
+            let v = e.target.href;
+            
+            // manually stored delimiters
+            let args = [
+                ["facebook.com/groups/", "/"],
+                ["/posts/",              "?"],
+            ];
+
+            // store URL length to detect when shortened
+            let l = v.length;
+
+            // split URL using parts and then add again to make URL excluding excess
+            for (let c=0; c<args.length; ++c) {
+                v = v.split(args[c][0]).length==2
+                    ? (v.split( args[c][0] )[0] + args[c][0] + v.split( args[c][0] )[1].split( args[c][1] )[0])
+                    : v;
+                if (v.length<l) break;
             }
+
+            // replace target URL [this needs to be done differently]
+            e.target.href = v;
+
         }
     }
 }
