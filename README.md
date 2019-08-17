d5e293b5faebdd0922163c488ee4d1646e77b883
nr072 <26773186+nr072@users.noreply.github.com>
2019-08-18 01:52:36 +0600

    add toggling functionality
    Turning link clipping on shows a small box hovering at bottom left corner.
    Also:
      - Change product name.
      - Reword comments.
      - Shorten clipping delimiter.
      - Modify README accordingly.

--- README.md
+++ README.md
@@ -1,23 +1,41 @@
-# qfls
+# Qlick
 
-QFLS (quite amateurishly, "Query-Full Link Shortener") is a link shortener used to prune long URLs full of annoying (to many) queries, especially those found on Facebook (like `facebook.com/posts/12341234?fb_click_id=xxxx`). 
+Qlick (shortened form of "Query-full Link Clipping Kit") is a URL clipper used to trim long URLs full of annoying (to many) queries, especially those found on Facebook (like `facebook.com/posts/12341234?fb_click_id=xxxx`).
 
 Facebook puts an awful lot of queries in its links. From notifications to outgoing URLs, they are everywhere. The outgoing ones are especially atrocious because of their `l.facebook.com/l.php?u=http://...` form. So, here is a little attempt to trim the extra queries and keep only the group names and post IDs.
 
 This way, `facebook.com/groups/myGroup/?multi_permalinks=xxxx&click_id=xxxx` becomes just `facebook.com/groups/myGroup/`.
 
-### How This Works
+### How It Works
 
-First, copy and paste the code in your browser terminal. You might want to go through the code before doing it, since pasting unknown stuff in a browser console is highly dangerous.
+First, copy and paste the code in your browser console. You might want to go through the code before doing it, since pasting unknown stuff in a browser console is highly dangerous.
 
 Middle-click on an HTML `<a>` tag and the shortened link opens in a new tab. Make sure to click directly on an `<a>` tag. Otherwise, it will not work – not even clicking on a descendant of an `<a>` tag. A trick to successfully target an `<a>` tag is to click on either of the white (bluish for unread) margin spaces on both the left and the right sides of a notification.
 
 What actually happens is, when you middle-click on an `<a>` tag, the target's URL is trimmed and then written to the tag's `href` attribute. And then it opens in a new tab. But the original (long) URL is saved in JS, and half a second after the click, the original URL is again written to the target. So, it appears as if just a trimmed down version of the URL has been opened in a new tab, nothing more.
 
+Once the code is pasted in the browser console, a small box will appear at the bottom left corner. This is just an indicator that the clipping has been turned on. If you paste the code in the console once again, clipping will be turned off, and the box will disappear. This portion of the code has future plans to becoming a browser extension.
+
 ### Upside
 
-Easy and quick. If Facebook link structures are changed, just modify the `args` variable in the JS file. Or add more as new link formats that have been missed before are found.
+Easy and quick. If Facebook link structures are changed, just modify the `args` variable in the JS file. Or add more as new link formats are noticed.
 
 ### Downside
 
 The content of a page is modified, although temporarily (until reload). This should be resolved.
+
+### Miscellaneous
+
+#### Is "URL clipper" a term?
+
+Not as far as I know. The term "link shortener" would have felt more natural here, but these days it has this pretty-established preconception of using a _separate_ short link that _redirects_ to the original link. But Qlick just trims a link down and opens that, there is no redirecting. So, used "clipper" instead.
+
+#### Is "query-full" a valid English word?
+
+No idea. I made that up. "Query-riddled" seemed a bit too long and dramatic.
+
+Maybe something better in the future – keeping the "query" part unchanged, obviously. But this will do for now.
+
+#### Why the "kit" in Qlick?
+
+A few more types of clipping facilities are planned. "Qlick" is actually the name of the entire... pack?
--- from-notifications.js
+++ from-notifications.js
@@ -1,43 +1,71 @@
-// copy-paste code in browser terminal
-//
-// middle-click on FB query-full long link to shorten to minimum
-// e.g., "fb.com/groups/myGroup/?multi_permalinks=xxxx&click_id=xxxx" to "fb.com/groups/myGroup/"
-// click's target must be HTML <a> tag only, not children
+// Copy-paste code in browser console.
+// (Paste again to turn it off.)
+
+// Middle-click on FB query-full long link to shorten to minimum.
+// E.g., "fb.com/groups/myGroup/?multi_permalinks=xxxx&click_id=xxxx" to "fb.com/groups/myGroup/".
+// Click's target must be HTML <a> tag only. Even descendants do not work.
 
 {
 
     "use strict";
 
+    // Keep track if link clipping is turned on or off.
+    let isQlickOn;
+    
+    // If turned off, create a marker to show it, after turning on. Otherwise,
+    // remove existing marker.
+    let marker = document.getElementById('qlick');
+    if (marker) {
+        document.body.removeChild(marker);
+        isQlickOn = false;
+    } else {
+        let el = document.createElement("div");
+        el.id                 = "qlick";
+        el.innerText          = "Qlick";
+        el.style.position     = "fixed";
+        el.style.bottom       = "15px";
+        el.style.left         = "15px";
+        el.style.color        = "#666";
+        el.style.border       = "1px solid #aaa";
+        el.style.borderRadius = "5px";
+        el.style.padding      = "3px 7px";
+        el.style.zIndex       = 9999;
+        document.body.appendChild(el);
+        isQlickOn = true;
+    }
+
+    // Clip target <a> tag's link if directly middle-clicked when turned on.
     document.onmousedown = function(e) {
-        
-        // detect middle-click on an HTML <a> tag that has a URL
-        if (e.which==2 && e.target.tagName=="A" && e.target.href) {
-            
-            // store original URL so it can be put back later
+        if (isQlickOn && e.which==2 && e.target.tagName=="A" && e.target.href) {
+            let u = e.target.href;
+            let l = u.length;
+
+            // Store original URL to put back in target later.
             let o = e.target.href;
 
-            let v = e.target.href;
-            
-            // manually stored delimiters
+            // Manually store delimiters for clipping.
             let args = [
-                ["facebook.com/groups/", "/"],
-                ["/posts/",              "?"],
+                ["/groups/", "/"],
+                ["/posts/" , "?"],
             ];
-
-            // store URL length to detect when shortened
-            let l = v.length;
-
-            // split URL using parts and then add again to make URL excluding excess
+            
+            // Loop until link is successfully clipped using suitable set of
+            // delimiters:
+            // Split URL using each delimiter set, and construct clipped URL by
+            // taking up to 2nd delimiter. If link length decreased, link was
+            // clipped successfully. Otherwise, apply next set.
             for (let c=0; c<args.length; ++c) {
-                v = v.split(args[c][0]).length==2
-                    ? (v.split( args[c][0] )[0] + args[c][0] + v.split( args[c][0] )[1].split( args[c][1] )[0])
-                    : v;
-                if (v.length<l) break;
+                u = u.split(args[c][0]).length==2
+                    ? (u.split( args[c][0] )[0] + args[c][0] + u.split( args[c][0] )[1].split( args[c][1] )[0])
+                    : u;
+                if (u.length<l)
+                    break;
             }
 
-            // replace target URL [this needs to be done differently]
-            e.target.href = v;
+            // Put clipped link in target, to open it as a result of middle-click.
+            e.target.href = u;
 
+            // Restore original link after delay (target has been clicked).
             setTimeout(function() {
                 e.target.href = o;
             }, 500);
