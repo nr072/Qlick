0d41bcc8cb51428e57ba28efbe0755a7825f7885
nr072 <26773186+nr072@users.noreply.github.com>
2020-02-01 15:17:21 +0600

    add whitespaces and comments, move functions to the end

--- mQ.js
+++ mQ.js
@@ -7,6 +7,7 @@
 // to "facebook.com/groups/myGroup/".
 
 
+
 "use strict";
 
 // Keep track if link clipping is turned on or off.
@@ -28,24 +29,74 @@ if (sign) {
     isQlickOn = true;
 }
 
-function createSign(conf) {
-    let sb = document.createElement("div");
-    sb.id                 = conf.id;
-    sb.innerText          = conf.text;
-    sb.style.position     = "fixed";
-    sb.style.bottom       = "15px";
-    sb.style.left         = "15px";
-    sb.style.color        = "#666";
-    sb.style.border       = "1px solid #aaa";
-    sb.style.borderRadius = "5px";
-    sb.style.padding      = "3px 7px";
-    sb.style.zIndex       = 9999;
-    return sb;
+// Clip target <a> tag's link if directly middle-clicked when turned on.
+document.onmousedown = function(e) {
+
+    if (isQlickOn && e.which==2) {
+
+        let target;
+
+        // Detect if click target is an eligible (has `href` attribute) HTML
+        // <a> tag, and set it as target.
+        // Otherwise, set nearest eligible ancestor <a> tag as target.
+        if (e.target.tagName=="A" && e.target.href) {
+            target = e.target;
+        } else {
+            let ancestor = e.target.closest('a[href]');
+            if (ancestor.href) {
+                target = ancestor;
+            }
+        }
+
+        // Put clipped link in target's `href`, and replace with old link
+        // again after delay.
+        if (target) {
+
+            // Store original URL to put back in target later.
+            let oldURL = target.href;
+
+            // Put clipped link in target, to open it as a result of the
+            // middle-click.
+            target.href = clipLink(oldURL);
+
+            // Restore original link after delay (presumably, after target
+            // has been clicked).
+            setTimeout(function() {
+                target.href = oldURL;
+            }, 500);
+
+        }
+
+    }
+
 }
 
+
+
+// Show a sign at lower left, saying "Qlick", indicating Qlick is turned
+// on.
+function createSign(conf) {
+    let sign = document.createElement("div");
+    sign.id                 = conf.id;
+    sign.innerText          = conf.text;
+    sign.style.position     = "fixed";
+    sign.style.bottom       = "15px";
+    sign.style.left         = "15px";
+    sign.style.color        = "#666";
+    sign.style.border       = "1px solid #aaa";
+    sign.style.borderRadius = "5px";
+    sign.style.padding      = "3px 7px";
+    sign.style.zIndex       = 9999;
+    return sign;
+}
+
+
+
+// Clip link based on delimiter sets. Try each set until one applies.
 function clipLink(url) {
 
-    // Store old URL length to later determine if link was clipped successfully.
+    // Store old URL length to later determine if link was clipped
+    // successfully.
     let oldLength  = url.length;
 
     // Delimiter sets for clipping
@@ -87,11 +138,11 @@ function clipLink(url) {
 
     ];
 
-    // Loop until link is successfully clipped using suitable set
-    // of delimiters.
-    // Split URL using each delimiter set, and construct clipped
-    // URL by taking up to 2nd delimiter. If link length decreases,
-    // link was clipped successfully. If not, apply next set.
+    // Loop until link is successfully clipped using suitable set of
+    // delimiters.
+    // Split URL using each delimiter set, and construct clipped URL by
+    // taking up to 2nd delimiter. If link length decreases, link was
+    // clipped successfully. If not, apply next set.
     for (let c=0; c<dList.length; ++c) {
 
         url = url.split(dList[c][0]).length==2
@@ -106,42 +157,5 @@ function clipLink(url) {
     }
 
     return url;
-}
 
-// Clip target <a> tag's link if directly middle-clicked when turned on.
-document.onmousedown = function(e) {
-
-    if (isQlickOn && e.which==2) {
-
-        let target;
-
-        // Detect if click target is an eligible (has `href` attribute) HTML
-        // <a> tag, and set it as target.
-        // Otherwise, set nearest eligible ancestor <a> tag as target.
-        if (e.target.tagName=="A" && e.target.href) {
-            target = e.target;
-        } else {
-            let ancestor = e.target.closest('a[href]');
-            if (ancestor.href) {
-                target = ancestor;
-            }
-        }
-
-        if (target) {
-
-            // Store original URL to put back in target later.
-            let oldURL = target.href;
-
-            // Put clipped link in target, to open it as a result of
-            // middle-click.
-            target.href = clipLink(oldURL);
-
-            // Restore original link after delay (target has been clicked).
-            setTimeout(function() {
-                target.href = oldURL;
-            }, 500);
-
-        }
-
-    }
 }
